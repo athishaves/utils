e35b896fbfd1aa8f26523e0d83725381d3631266
Athish Venkatesh <47027954+athishaves@users.noreply.github.com>
2021-04-06 12:30:02 +0530

    Split pdf using cli

--- splitPDF.py
+++ splitPDF.py
@@ -1,16 +1,28 @@
+# COMMAND LINE :    python filename.py INPUT_PDF OUTPUT_PDF [ [ page_a page_b ] ..... ]
+
+        
 from PyPDF2 import PdfFileWriter, PdfFileReader
+import sys
 
 
-# TODO : Pdf location
-inputPdf = PdfFileReader(open(pdf_location, "rb"))
+# Command Line arguments
+args = sys.argv
+print(str(args))
 
+
+# Pdf location
+inputPdf = PdfFileReader(open(args[1], "rb"))
 outputPdf = PdfFileWriter()
 
 
-# Range from page a to b (exclusive)
-for i in range (30, 39):
-    outputPdf.addPage(inputPdf.getPage(i))
-
-    # TODO : Result Pdf location
-    with open(result_pdf_location, "wb") as outputStream:
-        outputPdf.write(outputStream)
+def addPages (a, b):
+	for i in range (a,b+1):
+		outputPdf.addPage(inputPdf.getPage(i))
+		
+	    # Result Pdf location
+		with open(args[2], "wb") as outputStream:
+			outputPdf.write(outputStream)
+			
+			
+for i in range (3,len(args),2):
+	addPages(int(args[i]),int(args[i+1]))
